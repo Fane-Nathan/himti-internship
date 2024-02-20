094df82572a36c66063fa9432f68410ab5ce4cfa
Felix Nathaniel <felnat.surjo@gmail.com>
2024-02-20 12:00:04 +0700

    add the navbar animation

--- HIMTI_INTERNSHIP_SCRIPT.js
+++ HIMTI_INTERNSHIP_SCRIPT.js
@@ -1,44 +1,14 @@
-// Select all cards
-const cards = document.querySelectorAll(".card");
-
-// Function to rotate card
-function rotateCard(e) {
-  const cardRect = this.getBoundingClientRect();
-  const cardX = cardRect.left + cardRect.width / 2;
-  const cardY = cardRect.top + cardRect.height / 2;
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  const rotateY = (mouseX - cardX) / 10;
-  const rotateX = -(mouseY - cardY) / 10;
-  this.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-}
-
-// Add event listeners to each card
-cards.forEach((card) => {
-  card.addEventListener("mousemove", rotateCard);
-  card.addEventListener("mouseleave", function () {
-    this.style.transform = "rotateX(0) rotateY(0)";
-  });
-});
-
 let lastScrollTop = 0;
-let navbarTimeout;
 
 window.addEventListener('scroll', () => {
-  clearTimeout(navbarTimeout);
-
-  const navbar = document.querySelector('.NavBar'); // Changed from '.navbar' to '.NavBar'
+  const navbar = document.querySelector('.NavBar');
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollTop > lastScrollTop) {
-    navbar.classList.add('navbar-hidden');
+  if (scrollTop < lastScrollTop) {
+    navbar.style.top = '0';
   } else {
-    navbar.classList.remove('navbar-hidden');
+    navbar.style.top = '-160px'; // Replace '50px' with the height of your navbar
   }
 
-  lastScrollTop = scrollTop;
-
-  navbarTimeout = setTimeout(() => {
-    navbar.classList.add('navbar-hidden');
-  }, 3000); // 3 seconds
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
 });
