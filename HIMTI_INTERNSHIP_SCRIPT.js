54b7f8b6371a37572814d238df9358227407a8f5
Felix Nathaniel <felnat.surjo@gmail.com>
2024-02-22 11:51:53 +0700

    Fix event cards

--- HIMTI_INTERNSHIP_SCRIPT.js
+++ HIMTI_INTERNSHIP_SCRIPT.js
@@ -22,26 +22,27 @@ function toggleMenu() {
   }
 }
 
-let currentCard = 0;
-const cards = Array.from(document.querySelectorAll('.card-list .card'));
+// I want to create a button function but somehow it never works
+// let currentCard = 0;
+// const cards = Array.from(document.querySelectorAll('.card-list .card'));
 
-// Show the first card initially
-cards[currentCard].classList.add('active');
+// // Show the first card initially
+// cards[currentCard].classList.add('active');
 
-document.getElementById('prevBtn').addEventListener('click', function() {
-  // Hide the current card
-  cards[currentCard].classList.remove('active');
-  // Move to the previous card, or loop back to the last card if we're at the start
-  currentCard = (currentCard - 1 + cards.length) % cards.length;
-  // Show the new current card
-  cards[currentCard].classList.add('active');
-});
+// document.getElementById('prevBtn').addEventListener('click', function() {
+//   // Hide the current card
+//   cards[currentCard].classList.remove('active');
+//   // Move to the previous card, or loop back to the last card if we're at the start
+//   currentCard = (currentCard - 1 + cards.length) % cards.length;
+//   // Show the new current card
+//   cards[currentCard].classList.add('active');
+// });
 
-document.getElementById('nextBtn').addEventListener('click', function() {
-  // Hide the current card
-  cards[currentCard].classList.remove('active');
-  // Move to the next card, or loop back to the first card if we're at the end
-  currentCard = (currentCard + 1) % cards.length;
-  // Show the new current card
-  cards[currentCard].classList.add('active');
-});
+// document.getElementById('nextBtn').addEventListener('click', function() {
+//   // Hide the current card
+//   cards[currentCard].classList.remove('active');
+//   // Move to the next card, or loop back to the first card if we're at the end
+//   currentCard = (currentCard + 1) % cards.length;
+//   // Show the new current card
+//   cards[currentCard].classList.add('active');
+// });
